00d644373b6ffe148d26a6e133a14bedb4a4f795
ming.ma <ming.ma@yulore.com>
2025-02-22 17:38:05 +0800

    add @ToString for vos

--- src/main/java/com/yulore/ollama/vo/TaskStatus.java
+++ src/main/java/com/yulore/ollama/vo/TaskStatus.java
@@ -1,8 +1,10 @@
 package com.yulore.ollama.vo;
 
 import lombok.Builder;
+import lombok.ToString;
 
 @Builder
+@ToString
 public class TaskStatus {
     public String task_id;
     public String status;
